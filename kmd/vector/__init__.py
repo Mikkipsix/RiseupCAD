6274540268954d4999ad9216f5a0b4c0f306d6c2
Mikkipsix <76728139+Mikkipsix@users.noreply.github.com>
2026-08-10 21:52:52 +0300

    Enable dimension topology diagnostics

--- kmd/vector/__init__.py
+++ kmd/vector/__init__.py
@@ -2,5 +2,9 @@
 """Распознавание растровых чертежей в DXF."""
 from . import assemble, detect, page, pipeline, solve  # noqa: F401,E501
 from .pipeline import overlay_png, summary, to_dxf, vectorize  # noqa: F401
+from .topology_integration import install as _install_topology
+
+_install_topology(pipeline)
+vectorize = pipeline.vectorize
 
 __all__ = ["vectorize", "to_dxf", "overlay_png", "summary"]
