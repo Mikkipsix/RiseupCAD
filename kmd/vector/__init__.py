1652fc978dcebcb615781557769336cb505139f6
Mikkipsix <76728139+Mikkipsix@users.noreply.github.com>
2026-08-10 17:09:08 +0300

    Install repeat-supported calibration policy

--- kmd/vector/__init__.py
+++ kmd/vector/__init__.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 """Распознавание растровых чертежей в DXF."""
 from . import assemble, detect, page, pipeline, solve  # noqa: F401,E501
+from .calibration_policy import install as _install_calibration_policy
 from .pipeline import overlay_png, summary, to_dxf, vectorize  # noqa: F401
+
+_install_calibration_policy(solve)
 
 __all__ = ["vectorize", "to_dxf", "overlay_png", "summary"]
